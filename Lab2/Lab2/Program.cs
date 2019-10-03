57883b42b3a2b418b746854d37fc22ec8e04775d
reginapilip <regina.pilipchuk@gmail.com>
2019-10-03 15:18:01 -0700

    Thursday Demo 10.3.19 In-class lab

---
+++ Lab2/Lab2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SportsCar myCar = new SportsCar();
+            myCar.PetName = "my beatup Sienna";
+            Console.WriteLine("My car: {0}, \t{1}", myCar.getPetName(), myCar.PetName);
+        }
+    }
+}
